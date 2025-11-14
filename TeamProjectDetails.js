fa7cefe3e3d3b9052397e212383a8f8c1ba45ec1
Kielj1zzyyy <saguingaaronezekiel@gmail.com>
2025-11-14 14:07:31 +0800

    Added team project details // as main page

---
+++ TeamProjectDetails.js
@@ -0,0 +1,10 @@
+//This is main page // 
+
+//IT21 & IT24
+
+// Our project is UTECH-CARE , a web application admins to manage student logs, request checkups, and monitor available medicines in NBSC Clinic. //
+
+//Members
+//1. Aaron Ezekiel M. Saguing (Team Leader)
+//2. Alodia Zenny A. Balendez
+//3. John Anderson C. Cuizon 
